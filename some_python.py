b36a0f691f4ca1741d8a2758c97bca2086dbf485
Dimosthenis Schizas <dimosthenis.schizas@transifex.com>
2016-06-10 13:42:35 +0300

    Try branching

--- some_python.py
+++ some_python.py
@@ -1,8 +1,16 @@
-def add(a, b):
+def get(a, b):
     try:
         int(a)
 	int(b)
     except:
 	print "Enter numbers asshole"
 
-return a+b
+return a, b
+
+def add(a,b):
+    get(a, b)
+    return a+b
+
+def multi(a,b):
+    get(a, b)
+    return a*b
